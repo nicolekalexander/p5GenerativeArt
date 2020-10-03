c723c73b7064ddb7c0f5a5b8e3da69738c7d82ba
nicolekalexander <54696571+nicolekalexander@users.noreply.github.com>
2020-10-03 19:29:33 -0400

    Create circlesAtCursor.js

---
+++ circlesAtCursor.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  //background
+  background(0, 0, 0);
+}
+
+function draw() {
+  //ellipse
+  ellipse(mouseX, mouseY, 80, 80);
+  
+  //line
+  //stroke(126);
+  //line(mouseX, mouseY, 85, 75);
+}
